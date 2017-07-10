5173e6115b1464a726d231ccf4b2a2d4a29dce6f
Pedro J. Molina <pjmolina@gmail.com>
2017-07-10 11:47:59 +0200

    Added --allowed-start-rules to cli. v. 0.1.6

--- src/cli.js
+++ src/cli.js
@@ -22,6 +22,13 @@ args.map((arg, index) => {
         outFile = args[index + 1];
     }
 });
+let allowedStartRules = null;
+args.map((arg, index) => {
+    if (arg === "--allowed-start-rules") {
+        allowedStartRules = args[index + 1];
+    }
+});
+
 const trace = args.find(a => a === "--trace") ? true : false;
 const cache = args.find(a => a === "--cache") ? true : false;
 
@@ -30,22 +37,27 @@ function showHelp() {
     /* eslint-disable no-console */
     console.log("tspegjs v." + version + "      TS target for pegjs");
     console.log("Usage:");
-    console.log("  tspegjs [-o outFile.ts] [--trace] [--cache] <inGrammar.pegjs>");
+    console.log("  tspegjs [-o outFile.ts] [--allowed-start-rules <rule1,rule2>] [--trace] [--cache] <inGrammar.pegjs>");
 }
 
-function generateParser(input_file, output_file, trace, cache) {
+function generateParser(input_file, output_file, trace, cache, allowedStartRules) {
   fs.readFile(input_file, function (err, data) {
     if (err) throw err;
 
-    var parser = pegjs.generate(data.toString(), {
+    const opts = {
       output: "source",
       trace: trace,
       cache: cache,
       plugins: [tspegjs],
       tspegjs: {}
-    });
+    };
+    if (allowedStartRules) {
+        opts.allowedStartRules = allowedStartRules;
+    }
+
+    let parser = pegjs.generate(data.toString(), opts);
     fs.writeFileSync(output_file, parser);
   });
 }
 
-generateParser(inFile, outFile, trace, cache);
+generateParser(inFile, outFile, trace, cache, allowedStartRules);
